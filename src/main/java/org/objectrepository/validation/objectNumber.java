fba56c0de0fb6d25029db6f13625b120bbb9f598
Chris Roosendaal <chrisr@ogd.nl>
2012-07-18 17:51:07 +0200

    added ObjectNumber.java

---
+++ src/main/java/org/objectrepository/validation/objectNumber.java
@@ -0,0 +1,11 @@
+package org.objectrepository.validation;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cro
+ * Date: 18-7-12
+ * Time: 10:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class objectNumber {
+}
