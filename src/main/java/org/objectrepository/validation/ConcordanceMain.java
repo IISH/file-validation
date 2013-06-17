972f4437980405ae17c58f0addd6a0cdfe0054a0
Lucien van Wouw <lwo@iisg.nl>
2013-06-17 15:50:00 +0200

    Tweak argument validation

--- src/main/java/org/objectrepository/validation/ConcordanceMain.java
+++ src/main/java/org/objectrepository/validation/ConcordanceMain.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class ConcordanceMain {
 
-    private static final String USAGE = "Usage: java -fileSet [root directory containing tif, jpeg etc. directories] -prefix [project prefix] -na [PID prefix]";
+    private static final String USAGE = "Usage: java org.objectrepository.validation.ConcordanceMain -fileSet [/a/b/c/[naming authority]/root directory containing the dataset" ;
 
     private static final String[] EXPECT = {
             "-fileSet"
@@ -21,7 +21,7 @@ public class ConcordanceMain {
 
     /**
      * main
-     *
+     * <p/>
      * Start the validation using the fileSet:
      * /a/b/c/archivalID
      *
@@ -29,23 +29,14 @@ public class ConcordanceMain {
      */
     public static void main(String[] args) {
 
-        if (args.length < 4) {
-
-            System.out.println(USAGE);
-            System.exit(-1);
-        }
-
         Map<String, String> map = new HashMap(EXPECT.length);
-
         for (int i = 0; i < args.length; i += 2) {
             map.put(args[i], args[i + 1]);
         }
 
-
         for (String key : EXPECT) {
             if (!map.containsKey(key)) {
                 System.out.println("Expected case sensitive parameter: " + key + "\n");
-
                 System.out.println(USAGE);
                 System.exit(1);
             }
